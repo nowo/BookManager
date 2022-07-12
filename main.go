d4e9afd67a976cae05e8cc563c535c7369187bef
erdal <erdalburakcinar@gmail.com>
2022-07-12 17:34:31 +0300

    try push to master

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"BookManagementApp/handler"
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("server started at port 8080")
 	http.HandleFunc("/get", handler.GetUser)
 	http.HandleFunc("/create", handler.CreateUser)
 	http.ListenAndServe(":8080", nil)
